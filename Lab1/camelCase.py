1195447a78cb1715edcd5d083cf537b06a9d5575
Nelwell <nick.elwell206@gmail.com>
2021-01-13 02:43:07 -0600

    removed unecessary lowercase function line 10

--- Lab1/camelCase.py
+++ Lab1/camelCase.py
@@ -7,7 +7,7 @@ word_list = sentence.split() # turns sentence into list of words
 first_letters_capital = [] # empty list
 
 for word in word_list: # loops through each word in word_list
-    first_letters_capital.append(word.lower().title()) # appends each word with first letter capitalized
+    first_letters_capital.append(word.title()) # creates new list with first letter of each word capitalized
 
 list_to_single_string = ' '.join(first_letters_capital).replace(' ', '') # converts list of strings into single string and removes white space - https://stackoverflow.com/questions/36094979/convert-a-list-of-strings-into-one-string
 camel_case_sentence = list_to_single_string[0].lower() + list_to_single_string[1:] # capitalizes first letter of string - https://www.geeksforgeeks.org/python-lowercase-first-character-of-string/
