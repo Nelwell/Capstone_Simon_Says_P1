3c6a77ce20162e56d18fbb3b67a7d1d8a6963dfb
Nelwell <nick.elwell206@gmail.com>
2021-01-12 21:08:34 -0600

    final versions of warmup exercises

--- python_warmup.py
+++ python_warmup.py
@@ -2,12 +2,13 @@
 
 from datetime import datetime
 
-def get_name():
+def main():
     name = input('Hello! What is your name? ')
-    print (f'Good day to you,{name}! Now, let me ask you another question if I may...')
+    print (f'Good day to you, {name}!')
+    print(f'By the way, there are {len(name)} letters in your name.')
+    print('Now, let me ask you another question if I may...')
+    is_it_your_bday_month = check_bday_month_against_current_month(name)
     return name
-
-name = get_name()
 
 # def get_birthday_month():
 #     birthday_month = input(f'What month were you born in {name}? ').lower()
@@ -18,9 +19,7 @@ name = get_name()
 #         print('It\'s not quite your Birthday month yet.') 
 #     return birthday_month
 
-# birthday_month = get_birthday_month()
-
-def check_bday_month_against_current_month():
+def check_bday_month_against_current_month(name):
     birthday_month = input(f'What month were you born in {name}? ').lower()
     current_month = datetime.now().strftime('%B').lower() # Gets current month's name - courtesy https://discuss.codecademy.com/t/can-i-display-the-month-name-instead-of-a-number/337967
 
@@ -30,6 +29,4 @@ def check_bday_month_against_current_month():
         print('It\'s not quite your Birthday month yet.')
     return birthday_month
 
-is_it_your_bday_month = check_bday_month_against_current_month()
-
-print(f'Oh and by the way, there are {len(name)} letters in your name.')
+main()
