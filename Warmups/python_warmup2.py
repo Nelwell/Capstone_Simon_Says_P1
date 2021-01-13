3e1f8449a29fd2026aea83a310c9ef6ed54204af
Nelwell <nick.elwell206@gmail.com>
2021-01-12 23:30:40 -0600

    init

---
+++ Warmups/python_warmup2.py
@@ -0,0 +1,12 @@
+# This program asks the user for classes they are taking this semester, adds them to a list and displays them line by line
+
+classes = []
+
+class_name = input('Name a class you are taking this semester. If none, press Enter to exit. ')
+
+while class_name: # Continues until entered value is empty
+    classes.append(class_name)
+    class_name = input('Any other classes? Press Enter if finished. ')
+
+for index, c in enumerate(classes):
+    print(f'{index +1}. {c}')
