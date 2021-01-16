b1a52b09bd9a0d8cf102b612140532fd310c4731
Nelwell <nick.elwell206@gmail.com>
2021-01-16 12:07:42 -0600

    corrected syntax error line 10

--- simon_says_main.py
+++ simon_says_main.py
@@ -7,7 +7,7 @@ import random
 
 def main():
     colors = ['B', 'R', 'Y', 'G']  # possible colors in simon pattern
-    simon_pattern = [random.choice.colors]
+    simon_pattern = [random.choice(colors)]
     print(simon_pattern)
 
 
