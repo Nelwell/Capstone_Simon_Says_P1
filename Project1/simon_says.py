f7c71d04feb117a3a0be224a1891848af1e597f7
Nelwell <nick.elwell206@gmail.com>
2021-01-16 09:53:24 -0600

    initial push

---
+++ Project1/simon_says.py
@@ -0,0 +1,14 @@
+# This is a Simon Says, text-based game.
+# Follow Simon's pattern for as long as possible.
+# One error will result in game over.
+
+import random
+
+
+def main():
+    colors = ['B', 'R', 'Y', 'G'] # possible colors for simon pattern
+    simon_pattern = [random.choice.colors]
+    print(simon_pattern)
+
+
+main()
