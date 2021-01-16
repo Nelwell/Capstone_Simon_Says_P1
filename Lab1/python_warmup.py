6391f746ba9e175f2e84e70009dfbc19b2ef6e71
Nelwell <nick.elwell206@gmail.com>
2021-01-16 11:21:10 -0600

    updated line 11 to remove unused variable

--- Lab1/python_warmup.py
+++ Lab1/python_warmup.py
@@ -2,17 +2,19 @@
 
 from datetime import datetime
 
+
 def main():
     name = input('Hello! What is your name? ')
-    print (f'Good day to you, {name}!')
+    print(f'Good day to you, {name}!')
     print(f'By the way, there are {len(name)} letters in your name.')
     print('Now, let me ask you another question if I may...')
-    is_it_your_bday_month = check_bday_month_against_current_month(name)
+    check_bday_month_against_current_month(name)
     return name
+
 
 # def get_birthday_month():
 #     birthday_month = input(f'What month were you born in {name}? ').lower()
-    
+
 #     if birthday_month == 'august':
 #         print(f'Happy Birthday month to you, {name}!')
 #     else:
@@ -21,7 +23,9 @@ def main():
 
 def check_bday_month_against_current_month(name):
     birthday_month = input(f'What month were you born in {name}? ').lower()
-    current_month = datetime.now().strftime('%B').lower() # Gets current month's name - courtesy https://discuss.codecademy.com/t/can-i-display-the-month-name-instead-of-a-number/337967
+    current_month = datetime.now().strftime(
+        '%B').lower()  # Gets current month's name - courtesy
+    # https://discuss.codecademy.com/t/can-i-display-the-month-name-instead-of-a-number/337967
 
     if birthday_month == current_month:
         print(f'Happy Birthday month to you, {name}!')
@@ -29,4 +33,5 @@ def check_bday_month_against_current_month(name):
         print('It\'s not quite your Birthday month yet.')
     return birthday_month
 
-main() # Call main function
+
+main()  # Call main function
