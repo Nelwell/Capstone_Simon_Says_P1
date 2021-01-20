d6b0a9bb6a0061452e6c58d5b5df42b5d443ec5b
Nelwell <nick.elwell206@gmail.com>
2021-01-20 14:22:33 -0600

    pushing for error

--- Lab2/author_class.py
+++ Lab2/author_class.py
@@ -1,1 +1,32 @@
-# init
+# Author class program
+
+class Author:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def publish(self, title):
+        self.books.append(title)
+
+    def __str__(self):
+        # if self.books:  # resolves as true if the list is not empty
+        #     book_list = ', '.join(self.books)
+        # else:
+        #     book_list = 'No books'
+        book_list = ', '.join(self.books) or 'No published books'  # from slide example, sets to what's after "or" if
+        # first statement is false
+        return f'Author: {self.name}, Published books: {book_list}'
+
+
+def main():
+    rowling = Author('JK Rowling')
+    rowling.publish('Harry Potter')
+
+    rowling.publish('Fantastic Beasts')
+
+    elwell = Author('Nick Elwell')
+    print(rowling)
+    print(elwell)
+
+
+main()
