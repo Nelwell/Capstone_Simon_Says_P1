0d78921664af15afa62d2b612a55b1d5a3a7284d
Nelwell <nick.elwell206@gmail.com>
2021-01-25 08:02:46 -0600

    Completed. Added scoring with own function, play_again function, and other tweaks.

---
+++ SimonSaysP1/SimonSaysP1/pattern_builder.py
@@ -0,0 +1,29 @@
+import random
+import time
+import winsound
+from colorama import Fore, Style
+from text_formatting import Bold_text
+
+
+# logic to build Simon's pattern each round
+def pattern_builder(colors, simon_pattern):
+    # https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
+    simon_pattern.append(random.choice(colors))
+    for color in simon_pattern:
+        if color == 'R':
+            time.sleep(.8)  # delay before displaying next color
+            winsound.PlaySound('audio\\red_sound.wav', winsound.SND_ASYNC)  # plays called audio file
+            print(f'{Fore.RED}{Bold_text.BOLD}{color}{Style.BRIGHT}', end=' ')
+        elif color == 'G':
+            time.sleep(.8)
+            winsound.PlaySound('audio\\green_sound.wav', winsound.SND_ASYNC)
+            print(f'{Fore.GREEN}{Bold_text.BOLD}{color}{Style.BRIGHT}', end=' ')
+        elif color == 'B':
+            time.sleep(.8)
+            winsound.PlaySound('audio\\blue_sound.wav', winsound.SND_ASYNC)
+            print(f'{Fore.BLUE}{Bold_text.BOLD}{color}{Style.BRIGHT}', end=' ')
+        else:
+            time.sleep(.8)
+            winsound.PlaySound('audio\\yellow_sound.wav', winsound.SND_ASYNC)
+            print(f'{Fore.YELLOW}{Bold_text.BOLD}{color}{Style.BRIGHT}', end=' ')
+    return simon_pattern
